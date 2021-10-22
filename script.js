e173ee5858b9a0beb61d59b32ca781beb6941eb1
George <no email provided>
2021-10-22 19:30:30 +0100

    Hide nav to top button when footer is visible, so the sm icons are accessible

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const footer = document.querySelector("footer");
+const html = document.documentElement;
+
 const debounce = (fn) => {
   let frame;
   return (...params) => {
@@ -18,3 +21,16 @@ const storeScroll = () => {
 document.addEventListener('scroll', debounce(storeScroll), { passive: true });
 
 storeScroll();
+
+const storeFooterVisible = () => {
+  const footerRect = footer.getBoundingClientRect();
+  const visible = ( !!footerRect
+    && footerRect.bottom >= 0
+    && footerRect.right >= 0
+    && footerRect.top <= html.clientHeight
+    && footerRect.left <= html.clientWidth
+  );
+  document.documentElement.dataset.footer = visible;
+}
+
+document.addEventListener('scroll', debounce(storeFooterVisible), {passive: true});
